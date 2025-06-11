9ee3ef27ce6d2bccfc8cde3de812f9c18df624de
Serotini <dud112011@hotmail.com>
2025-06-11 15:21:11 -0300

    Criação de professor

--- src/App.java
+++ src/App.java
@@ -1,9 +1,12 @@
 import java.util.Scanner;
+import java.sql.*;
+import java.time.LocalDate;
 
 public class App {
     public static void main(String[] args) throws Exception {
         boolean x = true;
         Scanner scanner = new Scanner(System.in);
+        sqlConn conn = new sqlConn();
         do{
             System.out.println("MENU \n" +
                                "1. Cadastrar professores \n" +
@@ -14,16 +17,16 @@ public class App {
             int escolha = scanner.nextInt();
             switch (escolha) {
                 case 1:
-                    
+                    criaProfessor(scanner, conn);
                     break;
                 case 2:
-                    
+                    criaArea();
                     break;
                 case 3:
-                    
+                    alteraArea();
                     break;
                 case 4:
-                    
+                    criaCurso();
                     break;
                 case 5:
                     x = false;
@@ -35,22 +38,49 @@ public class App {
             }
         } while (x == true);
         scanner.close();
-        //sqlConn conn = new sqlConn();
     }
 
-    public void criaProfessor() {
+    public static void criaProfessor(Scanner scanner, sqlConn sqlConn) {
+        scanner.nextLine();
+
+        System.out.println("Digite o nome do professor:");
+        String nome = scanner.nextLine();
+        System.out.println("Digite a data de nascimento do professor:");
+        String dataNascimento = scanner.nextLine();
+        LocalDate data = LocalDate.parse(dataNascimento);
+        System.out.println("Digite o salário do professor:");
+        double salario = scanner.nextDouble();
+        System.out.println("Digite o número de cursos ministrados pelo professor:");
+        int curso = scanner.nextInt();
+
+        String sql = "INSERT INTO professores (nome, data, salario, cursosministrados) VALUES (?, ?, ?, ?)";
+
+        try(Connection conn = sqlConn.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, nome);
+            pstmt.setObject(2, data);
+            pstmt.setDouble(3, salario);
+            pstmt.setInt(4, curso);
+            int rowsAffected = pstmt.executeUpdate();
+            if (rowsAffected > 0) {
+                System.out.println("Professor cadastrado com sucesso!");
+            } else {
+                System.out.println("Erro ao cadastrar professor.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void criaArea() {
         
     }
 
-    public void criaArea() {
+    public static void alteraArea() {
         
     }
 
-    public void alteraArea() {
-        
-    }
-
-    public void criaCurso() {
+    public static void criaCurso() {
         
     }    
 }
--- src/sqlConn.java
+++ src/sqlConn.java
@@ -2,13 +2,28 @@ import java.sql.*;
 import java.util.Properties;
 
 public class sqlConn {
-    String url = "jdbc:postgresql://ep-green-thunder-a85vly69-pooler.eastus2.azure.neon.tech/neondb?user=neondb_owner&password=npg_z7gNFDP5lJim&sslmode=require";
+    String url = "jdbc:postgresql://ep-green-thunder-a85vly69-pooler.eastus2.azure.neon.tech/cursos?user=neondb_owner&password=npg_z7gNFDP5lJim&sslmode=require";
     Connection conn;
 
     public sqlConn() {
         try {
         conn = DriverManager.getConnection(url);
         System.out.println("Connected to the database successfully!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getConnection() {
+        return this.conn;
+    }
+
+    public void close() {
+        try {
+            if (conn != null && !conn.isClosed()) {
+                conn.close();
+                System.out.println("Conexão com o banco de dados fechada.");
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
